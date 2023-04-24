95eb24622815e7409aae30c9f4f83fa454779de1
ademola12345 <87110640+ademola12345@users.noreply.github.com>
2023-04-24 06:00:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const bitcore = require('bitcore-lib-dgb');
+const ethers = require('ethers');
+const { VercelRequest, VercelResponse } = require('@vercel/node');
 
-const createAccount = () => {
-  const privateKey = new bitcore.PrivateKey();
-  const address = privateKey.toAddress().toString();
-  return { privateKey: privateKey.toString(), address };
+const privateKey = process.env.PRIVATE_KEY; // Replace with your private key
+const providerUrl = 'https://rpc.thundercore.com';
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+
+module.exports = async (req, res) => {
+  try {
+    const { to, value } = req.query;
+    const wallet = new ethers.Wallet(privateKey, provider);
+    const tx = {
+      to: to,
+      value: ethers.utils.parseEther(value),
+    };
+    const txHash = await wallet.sendTransaction(tx);
+    res.status(200).json({ status: 'success', txHash: txHash.hash });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: 'error', message: error.message });
+  }
 };
-
-console.log(createAccount());
